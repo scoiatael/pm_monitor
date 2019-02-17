85359bde3ab0a17aa27e033d66e27c28ac64098e
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2019-02-17 17:01:59 +0100

    Make it work under esp

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,52 @@
 #include <Arduino.h>
-// #include "Seeed_BME280.h"
+#include "Seeed_BME280.h"
 
-// #define MQ_135_PIN 0
+BME280 bme280;
 
-// BME280 bme280;
+#define SDA 4
+#define SCL 5
 
-// void setup() {
-//   Serial.begin(9600);
-//   bme280.init();
-// }
-
-// struct sensorReading {
-//   float pressure;
-//   int airQuality;
-//   float temperature;
-//   float altitude;
-//   uint32_t humidity;
-// };
-
-// sensorReading getReading(BME280 bme280) {
-//   float pressure = bme280.getPressure();
-//   return sensorReading{
-//     pressure,
-//     analogRead(MQ_135_PIN),
-//     bme280.getTemperature(),
-//     bme280.calcAltitude(pressure),
-//     bme280.getHumidity()
-//   };
-// }
-
-// void serialDump(sensorReading reading) {
-//   Serial.print("{ \"airQuality\": ");
-//   Serial.print(reading.airQuality, DEC);
-//   Serial.print(", \"pressure\": ");
-//   Serial.print(reading.pressure);
-//   Serial.print(", \"temperature\": ");
-//   Serial.print(reading.temperature);
-//   Serial.print(", \"altitude\": ");
-//   Serial.print(reading.altitude);
-//   Serial.print(", \"humidity\": ");
-//   Serial.print(reading.humidity);
-//   Serial.println("}");
-// }
-
-// void loop() {
-//   serialDump(getReading(bme280));
-//   delay(1000);
-// }
-
-#define LED_ESP12 2
-#define LED_BUILTIN 16
 void setup() {
-  pinMode(LED_ESP12, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-} 
- 
-void loop() { 
-  digitalWrite(LED_ESP12, HIGH); 
-  digitalWrite(LED_BUILTIN, LOW); 
-  delay(900); 
-  digitalWrite(LED_ESP12, LOW);
-  digitalWrite(LED_BUILTIN, HIGH); 
-  delay(500);
-} 
+  Wire.begin(SDA);
+  Wire.setClock(SCL);
+  Serial.begin(9600);
+  bme280.init();
+}
+
+struct sensorReading {
+  float pressure;
+  int airQuality;
+  float temperature;
+  float altitude;
+  uint32_t humidity;
+};
+
+sensorReading getReading(BME280 bme280) {
+  float pressure = bme280.getPressure();
+  return sensorReading{
+    pressure,
+    analogRead(A0),
+    bme280.getTemperature(),
+    bme280.calcAltitude(pressure),
+    bme280.getHumidity()
+  };
+}
+
+void serialDump(sensorReading reading) {
+  Serial.print("{ \"airQuality\": ");
+  Serial.print(reading.airQuality, DEC);
+  Serial.print(", \"pressure\": ");
+  Serial.print(reading.pressure);
+  Serial.print(", \"temperature\": ");
+  Serial.print(reading.temperature);
+  Serial.print(", \"altitude\": ");
+  Serial.print(reading.altitude);
+  Serial.print(", \"humidity\": ");
+  Serial.print(reading.humidity);
+  Serial.println("}");
+}
+
+void loop() {
+  serialDump(getReading(bme280));
+  delay(1000);
+}
